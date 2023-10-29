606e5250d986df2a95b8c75c943cf1aa11ce2c2a
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2023-10-30 08:53:13 +0900

    feat: 引数に範囲を追加

--- src/03/04/index.ts
+++ src/03/04/index.ts
@@ -1,4 +1,10 @@
 export function add(a: number, b: number) {
+  if (a < 0 || 100 < a) {
+    throw new Error("入力値は0~100の範囲で入力してください");
+  }
+  if (b < 0 || 100 < b) {
+    throw new Error("入力値は0~100の範囲で入力してください");
+  }
   const sum = a + b;
   if (sum > 100) {
     return 100;
