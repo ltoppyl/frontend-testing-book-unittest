b778cff0d41afd9a1bf93320d07ec46cd646e7df
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2023-10-30 09:01:44 +0900

    refactor: 引数のエラー判定の関数を共通化

--- src/03/04/index.ts
+++ src/03/04/index.ts
@@ -1,10 +1,13 @@
+const checkArgRange = (value: number) => {
+  if (value < 0 || 100 < value) {
+    throw new RangeError("入力値は0~100の範囲で入力してください");
+  }
+};
+
 export function add(a: number, b: number) {
-  if (a < 0 || 100 < a) {
-    throw new Error("入力値は0~100の範囲で入力してください");
-  }
-  if (b < 0 || 100 < b) {
-    throw new Error("入力値は0~100の範囲で入力してください");
-  }
+  checkArgRange(a);
+  checkArgRange(b);
+
   const sum = a + b;
   if (sum > 100) {
     return 100;
