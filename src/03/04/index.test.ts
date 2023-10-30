5960849eb475ac8fc7ec32ab557a7adf0b0ab5bd
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2023-10-30 09:03:35 +0900

    test: 内容のリファクタリング

--- src/03/04/index.test.ts
+++ src/03/04/index.test.ts
@@ -2,21 +2,27 @@ import { add, sub } from ".";
 
 describe("四則演算", () => {
   describe("add", () => {
-    test("返り値は、第一引数と第二引数の「和」である", () => {
-      expect(add(50, 50)).toBe(100);
+    describe("引数の範囲が不正な場合にエラーを投げる", () => {
+      test("引数aの範囲は、'0~100'である", () => {
+        expect(() => add(-1, 50)).toThrow(RangeError);
+        expect(() => add(50, 101)).toThrow(RangeError);
+      });
+      test("引数bの範囲は、'0~100'である", () => {
+        expect(() => add(-1, 50)).toThrow(RangeError);
+        expect(() => add(101, 50)).toThrow(RangeError);
+      });
     });
-    test("合計の上限は、'100'である", () => {
-      expect(add(70, 80)).toBe(100);
-    });
-    test("引数aの範囲は、'0~100'である", () => {
-      expect(() => add(-1, 50)).toThrowError("入力値は0~100の範囲で入力してください");
-      expect(() => add(101, 50)).toThrowError("入力値は0~100の範囲で入力してください");
-    });
-    test("引数bの範囲は、'0~100'である", () => {
-      expect(() => add(-1, 50)).toThrowError("入力値は0~100の範囲で入力してください");
-      expect(() => add(50, 101)).toThrowError("入力値は0~100の範囲で入力してください");
+
+    describe("引数の範囲が正常な場合に適切に計算を行う", () => {
+      test("返り値は、第一引数と第二引数の「和」である", () => {
+        expect(add(50, 50)).toBe(100);
+      });
+      test("合計の上限は、'100'である", () => {
+        expect(add(70, 80)).toBe(100);
+      });
     });
   });
+
   describe("sub", () => {
     test("返り値は、第一引数と第二引数の「差」である", () => {
       expect(sub(51, 50)).toBe(1);
