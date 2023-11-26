f17b187bf80ae9d114a76ec6211cc310d902bfa7
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2023-11-26 15:28:46 +0900

    test: チェックボックスがチェックできることを確認するテストを追加

--- src/05/05/Agreement.test.tsx
+++ src/05/05/Agreement.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Agreement } from "./Agreement";
+
+const user = userEvent.setup();
 
 test("fieldset のアクセシブルネームは、legend を引用している", () => {
   render(<Agreement />);
-  expect(
-    screen.getByRole("group", { name: "利用規約の同意" })
-  ).toBeInTheDocument();
+  expect(screen.getByRole("group", { name: "利用規約の同意" })).toBeInTheDocument();
 });
 
 test("チェックボックスはチェックが入っていない", () => {
@@ -18,10 +19,18 @@ test("利用規約へのリンクがある", () => {
   expect(screen.getByRole("link")).toBeInTheDocument();
   expect(screen.getByRole("link")).toHaveTextContent("利用規約");
   expect(screen.getByRole("link")).toHaveAttribute("href", "/terms");
-  expect(screen.getByRole("link", { name: "利用規約" })).toHaveAttribute(
-    "href",
-    "/terms"
-  );
+  expect(screen.getByRole("link", { name: "利用規約" })).toHaveAttribute("href", "/terms");
+});
+
+test("ユーザーがチェックボックスにチェックを入れることができる", async () => {
+  render(<Agreement />);
+  const checkbox = screen.getByRole("checkbox");
+
+  expect(checkbox).not.toBeChecked();
+
+  // ユーザーがチェックボックスにチェックする動作をモック
+  await user.click(checkbox);
+  expect(checkbox).toBeChecked();
 });
 
 test("Snapshot: 利用規約の同意が表示される", () => {
